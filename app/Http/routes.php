c4fe719b5085b236da882b7f883fb20587a2553e
chenqin <13224@qq.com>
2017-02-23 09:46:50 +0800

    aaaaddd

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,24 @@
 |
 */
 
-<<<<<<< HEAD
 
 
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/admin','admin\AdminController@index');
+// 用户模块控制器
+Route::controller('/admin/user','admin\UserController');
+// 友情链接控制器
+Route::controller('/admin/flink','admin\FlinkController');
+//商品管理控制器
+Route::controller('/admin/goods','admin\GoodsController');
+//分类管理控制器
+Route::controller('/admin/cate','admin\CateController');
+//前台商品搜索页面控制器
+Route::controller('/home/search','home\SearchController');
+//前台商品详情页面控制器
+Route::controller('/home/introduction','home\IntroductionController');
 
 
 
@@ -101,14 +116,12 @@
 
 //后台管理
 Route::get('/admin','admin\AdminController@index');
-//后台用户管理
-Route::controller('/admin/user','admin\UserController');
+
 //后台分类
 Route::controller('/admin/cate','admin\CateController');
 //轮播图
 Route::controller('/admin/lunbotu','admin\LunbotuController');
-//评论管理
-Route::controller('/home/comment','home\CommentController');
+
 
 
 
@@ -218,19 +231,6 @@ Route::controller('/home/comment','home\CommentController');
 
 
 Route::controller('/home/home','home\ZhuyeController');
-=======
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/admin','admin\AdminController@index');
-// 用户模块控制器
-Route::controller('/admin/user','admin\UserController');
-// 友情链接控制器
-Route::controller('/admin/flink','admin\FlinkController');
-//商品管理控制器
-Route::controller('/admin/goods','admin\GoodsController');
-//分类管理控制器
-Route::controller('/admin/cate','admin\CateController');
-//前台商品搜索页面控制器
-Route::controller('/home/search','home\SearchController');
->>>>>>> 81108adacfa1a97c92281fd48eef072bdc4fba00
+
+
+
